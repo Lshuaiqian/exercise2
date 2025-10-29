59f343e86afd17db4c2bc2328afa42516bbc0358
Lsq <2398989988@qq.com>
2025-10-29 22:41:04 +0800

    练习10.28

---
+++ iffor/advance/means/src/verification.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+public class verification {
+    public static void main(String[] args) {
+       System.out.println(v());
+       System.out.println("请输入验证码");
+       Scanner sc = new Scanner(System.in);
+       String input = sc.nextLine();
+       if(input.equals(v())) {
+           System.out.println("验证成功");
+       }else{
+           System.out.println("验证失败");
+       }
+    }
+    //一个生成验证码的方法
+    public static String  v(){
+        String result="";
+        char[] chas=new char[52];//所有字母放在一个数组中
+        for (int i = 0; i < chas.length; i++) {
+            if (i<26){
+                chas[i]=(char)(i+65);//通过强转ASCII码获得所有大写字母
+            }else {
+                chas[i]=(char)(i+97-26);//获得所有小写字母
+            }
+        }
+        Random r=new Random();
+        for (int i = 0; i <4 ; i++) {
+            int random=r.nextInt(chas.length);
+            result=result+chas[random];//把四个随机索引到的字母拼接
+        }
+        Random r2=new Random();//随机获取一个0到9的数字
+        int number=r2.nextInt(10);
+        //如何把这个随机数字与字母拼接
+        result=result+(char)('0'+number);
+        return result;
+    }
+}
